8f84a36ab2116093a05c4232be27d8748732c236
Jan Techner <jantechner@live.com>
2019-06-24 12:34:32 +0200

    Fix ports conflict

--- server/server.js
+++ server/server.js
@@ -1,42 +1,3 @@
-// const http = require('http');
-// const url_parser = require('url')
-
-// const server = http.createServer((request, response) => {
-
-//     // const { url, method, headers } = request;
-//     // console.log(url, method, headers)
-//     // const parsedUrl = url_parser.parse(url, true);
-//     // console.log(parsedUrl)
-//     // response.setHeader('content-type', 'text/plain');
-//     // response.write(`Your browser: ${headers['user-agent']}\n`);
-//     // response.write(`HTTP method: ${method}\n`);
-//     // response.write(`id: ${parsedUrl.query.id}`);
-//     // response.end();
-
-//     const body = [];
-
-//     request.on('error', (error) => {
-
-//         console.error(error.stack);
-//     });
-
-//     request.on('data', (chunk) => {
-//         console.log(chunk)
-//         body.push(chunk);
-//     });
-
-//     request.on('end', () => {
-//         const fullBody = Buffer.concat(body).toString();
-//         // console.log(fullBody)
-//         response.setHeader('content-type', 'text/plain');
-//         response.end(`This was posted: ${fullBody}`);
-//     });
-// });
-
-// server.listen(3000, () => {
-//     console.log('Server listening at http://localhost:3000/');
-// });
-
 const express = require('express')
 const fs = require('fs')
 const cors = require('cors')
@@ -52,6 +13,6 @@ app.use(cors())
 app.use('/api/notes', notes)
 app.use('/api/users', users)
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+app.listen(3030, () => {
+    console.log('Server running at http://localhost:3030/');
 });
